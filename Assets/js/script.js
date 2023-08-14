f0ad90ff593df336dac0e6d508740ba2d81b9e27
AmberDiehl1 <amberdiehl@outlook.com>
2023-08-13 22:42:42 -0500

    final edits priot to readme

---
+++ Assets/js/script.js
@@ -0,0 +1,135 @@
+var apiKEY = '3e652e3608b552ad8045b0052f8b4275'
+
+var cityTxt = document.getElementById('cityInput');
+var searchButton = document.getElementById('searchBtn')
+var actualDay = document.querySelector('.currentDay');
+var forcasts = document.querySelector('.fiveDayForcast');
+var searchedCity = document.getElementById('previousCities');
+
+document.addEventListener('DOMContentLoaded', () => {
+    var cities = JSON.parse(localStorage.getItem('searchedCities'))|| [];
+    for (var index = 0; index < cities.length; index++) {
+        createBtn(cities[index]);
+    }
+ })
+
+ function createBtn(name) {
+    const element = document.createElement('button');
+        element.textContent = name;
+        element.classList.add('typedCity');
+        searchedCity.append(element);
+ }
+
+function getCoordinates(coordinateApi) {
+    fetch(coordinateApi)
+        .then((response) => {
+    
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response);
+            actualWeather(response.coord.lat,response.coord.lon)
+            currentWeather(response);
+            // get current weather and create a separate function
+        });
+        
+    }
+    
+    function actualWeather(lat,lon) {
+        var weatherApi = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid='+ apiKEY + '&units=imperial'
+        fetch(weatherApi)
+        .then((response) => {
+            
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response);
+            showforecast(response.list);
+        });
+
+    }
+
+    function currentWeather(weather){
+     actualDay.textContent = '';
+    var searchName = weather.name;
+    var temp = weather.main.temp;
+    var date = weather.dt;
+    var iconurl = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+    var humidity = weather.main.humidity;
+    var wind = weather.wind.speed;
+    var container = document.createElement('div')
+    var cityName = document.createElement('div')
+    cityName.textContent ='City:' + ' ' + searchName;
+    var temperature = document.createElement('div')
+    temperature.textContent = 'Temperature:' + ' ' + temp;
+    var currentDate = document.createElement('div')
+    currentDate.textContent = new Date(date*1000).toLocaleDateString('en-US');
+    var windSpeed = document.createElement('div')
+    windSpeed.textContent = 'Wind:' + ' ' + wind;
+    var humidityDeg = document.createElement('div')
+    humidityDeg.textContent = 'Humidity:' + ' ' + humidity;
+    var emoji = document.createElement('img')
+    emoji.src = iconurl;
+    
+    container.append(cityName,currentDate,emoji,temperature,windSpeed,humidityDeg);
+    
+    actualDay.append(container);
+    actualDay.style.display = 'flex';
+    }
+
+    function showforecast (forcastArray){
+        forcasts.textContent = '';
+        for (let index = 8; index < forcastArray.length; index+=8) {
+            const element = forcastArray[index];
+            console.log(element);
+            var temp = element.main.temp;
+            var date = element.dt;
+            var iconurl = "http://openweathermap.org/img/w/" + element.weather[0].icon + ".png";
+            var humidity = element.main.humidity;
+            var wind = element.wind.speed;
+            var container = document.createElement('div')
+            var temperature = document.createElement('div')
+            temperature.textContent = 'Temperature:' + ' ' + temp;
+            var currentDate = document.createElement('div')
+            currentDate.textContent = new Date(date*1000).toLocaleDateString('en-US');
+            var windSpeed = document.createElement('div')
+            windSpeed.textContent = 'Wind:' + ' ' + wind;
+            var humidityDeg = document.createElement('div')
+            humidityDeg.textContent = 'Humidity:' + ' ' + humidity;
+            var emoji = document.createElement('img')
+            emoji.src = iconurl;
+            
+            container.append(currentDate,emoji,temperature,windSpeed,humidityDeg);
+            
+            forcasts.append(container);
+        }
+    }
+    searchButton.addEventListener('click', function () {
+        var textInput = cityTxt.value
+        var cities = JSON.parse(localStorage.getItem('searchedCities'))|| [];
+        var previousCityBtns = document.getElementsByClassName('typedCity');
+        console.log(previousCityBtns);
+        
+        if (previousCityBtns.length >= 5){
+            previousCityBtns[0].remove();
+            cities.shift();
+        }
+        
+        createBtn(textInput);
+        cities.push(textInput);
+        localStorage.setItem('searchedCities',JSON.stringify(cities))
+        var coordinateApi = `https://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=${apiKEY}&units=imperial`;
+        getCoordinates(coordinateApi);
+        
+    });
+    
+    
+    // add an eventlistener to all the prev city buttons that display the weather data for each of those cities. 
+    
+    searchedCity.addEventListener('click', function(event){
+        cityTxt.value = '';
+        if(event.target.classList.contains('typedCity')) {
+            getCoordinates(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.innerHTML}&appid=${apiKEY}&units=imperial`);
+            
+        }
+    })
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Weekly-Weather-Checks
+
+## Description
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+- What was your motivation?
+    - To create a weather dashboard in hope of seeing forcasts for weather conditions.
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+    - Wanted to gain proficiency with JavaScript and utilize the API concepts to further advance my knowledge of JS.
+- What problem does it solve?
+    - Being able to see the forcasted weather of any city and retain information for those cities at my personal whim.
+- What did you learn?
+    - I need to practice more. And inputing APIs are a difficult task.
+
+
+## Installation
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+- Create a github repo with a unique name
+- clone the https github repo URL into git using the git clone function
+type in "code ." into git once the repo is saved on your computer
+- VSCode will pop up onto your desktop and within VSCode you have the ability to edit and save committed material within VSCode.
+- This is communicating with Git when you are complete with changes utilize "git add ." to sync all changes
+- Use "git commit -m "enter commit messages" to commit the changes into git
+- Use "git push origin main" as the final push into github where you will see all of your saved changes and a copy of the code
+
+## Usage
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+List your collaborators, if any, with links to their GitHub profiles.
